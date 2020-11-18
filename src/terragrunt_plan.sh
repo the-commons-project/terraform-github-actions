21e2d98d97ccfbe3568dd9ac9fa300b673548000
Serge Ohl <serge.ohl@unity3d.com>
2020-11-18 14:41:29 -0500

    test

--- src/terragrunt_plan.sh
+++ src/terragrunt_plan.sh
@@ -3,7 +3,7 @@
 function terragruntPlan {
   # Gather the output of `terragrunt plan`.
   echo "plan: info: planning Terragrunt configuration in ${tfWorkingDir}"
-  planOutput=$(${tfBinary} --terragrunt-non-interactive -detailed-exitcode -input=false ${*} 2>&1)
+  planOutput=$(${tfBinary} ${*} -detailed-exitcode -input=false 2>&1)
   planExitCode=${?}
   planHasChanges=false
   planCommentStatus="Failed"

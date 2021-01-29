6ff9a604263ab4b6cf28e3c5c862b242d36891fd
David Gethings <David Gethings david.gethings@modusbox.com>
2021-01-29 17:18:30 +0000

    initial test of tg show command support

--- src/main.sh
+++ src/main.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-function stripColors {
+function stripColors() {
   echo "${1}" | sed 's/\x1b\[[0-9;]*m//g'
 }
 
-function hasPrefix {
+function hasPrefix() {
   case ${2} in
-    "${1}"*)
-      true
-      ;;
-    *)
-      false
-      ;;
+  "${1}"*)
+    true
+    ;;
+  *)
+    false
+    ;;
   esac
 }
 
-function parseInputs {
+function parseInputs() {
   # Required inputs
   if [ "${INPUT_TF_ACTIONS_VERSION}" != "" ]; then
     tfVersion=${INPUT_TF_ACTIONS_VERSION}
@@ -75,9 +75,9 @@ function parseInputs {
   fi
 }
 
-function configureCLICredentials {
+function configureCLICredentials() {
   if [[ ! -f "${HOME}/.terraformrc" ]] && [[ "${tfCLICredentialsToken}" != "" ]]; then
-    cat > ${HOME}/.terraformrc << EOF
+    cat >${HOME}/.terraformrc <<EOF
 credentials "${tfCLICredentialsHostname}" {
   token = "${tfCLICredentialsToken}"
 }
@@ -85,7 +85,7 @@ EOF
   fi
 }
 
-function installTerraform {
+function installTerraform() {
   if [[ "${tfVersion}" == "latest" ]]; then
     echo "Checking the latest version of Terraform"
     tfVersion=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].version' | grep -v '[-].*' | sort -rV | head -n 1)
@@ -107,7 +107,7 @@ function installTerraform {
   echo "Successfully downloaded Terraform v${tfVersion}"
 
   echo "Unzipping Terraform v${tfVersion}"
-  unzip -d /usr/local/bin /tmp/terraform_${tfVersion} &> /dev/null
+  unzip -d /usr/local/bin /tmp/terraform_${tfVersion} &>/dev/null
   if [ "${?}" -ne 0 ]; then
     echo "Failed to unzip Terraform v${tfVersion}"
     exit 1
@@ -115,7 +115,7 @@ function installTerraform {
   echo "Successfully unzipped Terraform v${tfVersion}"
 }
 
-function installTerragrunt {
+function installTerragrunt() {
   if [[ "${tgVersion}" == "latest" ]]; then
     echo "Checking the latest version of Terragrunt"
     latestURL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/gruntwork-io/terragrunt/releases/latest)
@@ -139,7 +139,7 @@ function installTerragrunt {
 
   echo "Moving Terragrunt ${tgVersion} to PATH"
   chmod +x /tmp/terragrunt
-  mv /tmp/terragrunt /usr/local/bin/terragrunt 
+  mv /tmp/terragrunt /usr/local/bin/terragrunt
   if [ "${?}" -ne 0 ]; then
     echo "Failed to move Terragrunt ${tgVersion}"
     exit 1
@@ -147,7 +147,7 @@ function installTerragrunt {
   echo "Successfully moved Terragrunt ${tgVersion}"
 }
 
-function main {
+function main() {
   # Source the other files to gain access to their functions
   scriptDir=$(dirname ${0})
   source ${scriptDir}/terragrunt_fmt.sh
@@ -159,6 +159,7 @@ function main {
   source ${scriptDir}/terragrunt_import.sh
   source ${scriptDir}/terragrunt_taint.sh
   source ${scriptDir}/terragrunt_destroy.sh
+  source ${scriptDir}/terragrunt_show.sh
 
   parseInputs
   configureCLICredentials
@@ -166,46 +167,50 @@ function main {
   cd ${GITHUB_WORKSPACE}/${tfWorkingDir}
 
   case "${tfSubcommand}" in
-    fmt)
-      installTerragrunt
-      terragruntFmt ${*}
-      ;;
-    init)
-      installTerragrunt
-      terragruntInit ${*}
-      ;;
-    validate)
-      installTerragrunt
-      terragruntValidate ${*}
-      ;;
-    plan)
-      installTerragrunt
-      terragruntPlan ${*}
-      ;;
-    apply)
-      installTerragrunt
-      terragruntApply ${*}
-      ;;
-    output)
-      installTerragrunt
-      terragruntOutput ${*}
-      ;;
-    import)
-      installTerragrunt
-      terragruntImport ${*}
-      ;;
-    taint)
-      installTerragrunt
-      terragruntTaint ${*}
-      ;;
-    destroy)
-      installTerragrunt
-      terragruntDestroy ${*}
-      ;;
-    *)
-      echo "Error: Must provide a valid value for terragrunt_subcommand"
-      exit 1
-      ;;
+  fmt)
+    installTerragrunt
+    terragruntFmt ${*}
+    ;;
+  init)
+    installTerragrunt
+    terragruntInit ${*}
+    ;;
+  validate)
+    installTerragrunt
+    terragruntValidate ${*}
+    ;;
+  plan)
+    installTerragrunt
+    terragruntPlan ${*}
+    ;;
+  show)
+    installTerragrunt
+    terragruntShow ${*}
+    ;;
+  apply)
+    installTerragrunt
+    terragruntApply ${*}
+    ;;
+  output)
+    installTerragrunt
+    terragruntOutput ${*}
+    ;;
+  import)
+    installTerragrunt
+    terragruntImport ${*}
+    ;;
+  taint)
+    installTerragrunt
+    terragruntTaint ${*}
+    ;;
+  destroy)
+    installTerragrunt
+    terragruntDestroy ${*}
+    ;;
+  *)
+    echo "Error: Must provide a valid value for terragrunt_subcommand"
+    exit 1
+    ;;
   esac
 }
 
---
+++ src/terragrunt_show.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+function terragruntShow() {
+  # Gather the output of `terragrunt plan`.
+  echo "show: info: showing Terragrunt configuration in ${tfWorkingDir}"
+  showOutput=$(${tfBinary} show -detailed-exitcode -no-color ${*} 2>&1)
+  showExitCode=${?}
+  showCommentStatus="Failed"
+
+  # Exit code of 0 indicates success with no changes. Print the output and exit.
+  if [ ${showExitCode} -eq 0 ]; then
+    echo "show: info: successfully showed Terragrunt configuration in ${tfWorkingDir}"
+    echo "${showOutput}"
+    echo
+    exit ${showExitCode}
+  fi
+
+  # Exit code of !0 indicates failure.
+  if [ ${showExitCode} -ne 0 ]; then
+    echo "show: error: failed to show Terragrunt configuration in ${tfWorkingDir}"
+    echo "${showOutput}"
+    echo
+  fi
+
+  # Comment on the pull request if necessary.
+  if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${tfComment}" == "1" ] && [ "${showCommentStatus}" == "Failed" ]; then
+    showCommentWrapper="#### \`${tfBinary} show\` ${showCommentStatus}
+<details><summary>Show Output</summary>
+
+\`\`\`
+${showOutput}
+\`\`\`
+
+</details>
+
+*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${tfWorkingDir}\`, Workspace: \`${tfWorkspace}\`*"
+
+    showCommentWrapper=$(stripColors "${showCommentWrapper}")
+    echo "show: info: creating JSON"
+    showPayload=$(echo "${showCommentWrapper}" | jq -R --slurp '{body: .}')
+    showCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+    echo "show: info: commenting on the pull request"
+    echo "${showPayload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${showCommentsURL}" >/dev/null
+  fi
+
+  # https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/m-p/38372/highlight/true#M3322
+  showOutput="${showOutput//'%'/'%25'}"
+  showOutput="${showOutput//$'\n'/'%0A'}"
+  showOutput="${showOutput//$'\r'/'%0D'}"
+
+  echo "::set-output name=tf_actions_show_output::${showOutput}"
+  exit ${showExitCode}
+}

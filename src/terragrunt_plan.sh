e9e39bf67ac5c37b8ff680748f26a7da1e8971e2
bokeneko <>
2021-06-29 11:36:18 +0900

    change output processing

--- src/terragrunt_plan.sh
+++ src/terragrunt_plan.sh
@@ -26,9 +26,7 @@ function terragruntPlan {
     echo "plan: info: successfully planned Terragrunt configuration in ${tfWorkingDir}"
     echo "${planOutput}"
     echo
-    if echo "${planOutput}" | egrep '^-{72}$' &> /dev/null; then
-        planOutput=$(echo "${planOutput}" | sed -n -r '/-{72}/,/-{72}/{ /-{72}/d; p }')
-    fi
+    planOutput=$(echo "${planOutput}" | sed -n -r '/-{72}/,$!p')
     planOutput=$(echo "${planOutput}" | sed -r -e 's/^  \+/\+/g' | sed -r -e 's/^  ~/~/g' | sed -r -e 's/^  -/-/g')
 
      # If output is longer than max length (65536 characters), keep last part
